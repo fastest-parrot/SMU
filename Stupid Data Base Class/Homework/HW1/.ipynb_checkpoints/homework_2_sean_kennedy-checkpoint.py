45fd622c29a9e8b060dd14ab2b0ef8ac108bcb4a
Sean Kennedy <skennedy@steadfast.com>
2019-09-10 16:01:23 -0400

    no message

---
+++ Stupid Data Base Class/Homework/HW1/.ipynb_checkpoints/homework_2_sean_kennedy-checkpoint.py
@@ -0,0 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# + {"pycharm": {"is_executing": false}}
+import pyodbc as db
+# -
+
+# ## Connect to DB
+#
+
+# + {"pycharm": {}}
+conn = db.conn()
+# -
+
+help(db.connect
+    )
+
+
